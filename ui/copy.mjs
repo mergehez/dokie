bcbf349b0d7a35c6b57f9a341ad8d972fc692c8e
mergehez <argesozdogan@gmail.com>
2025-06-10 19:17:29 +0200

    use script to copy html files

---
+++ ui/copy.mjs
@@ -0,0 +1,10 @@
+import * as fs from "node:fs";
+
+['../aspnetcore/wwwroot/dokie.html', '../demo/public/dokie.html'].forEach(file => {
+    try {
+        fs.rmSync(file, {force: true});
+        fs.cpSync('./dist/index.html', file, {force: true});
+    } catch (e) {
+        console.error(`Failed to remove file ${file}:`, e);
+    }
+})
