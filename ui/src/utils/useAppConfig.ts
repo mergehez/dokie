171fa5799bf2b198d1391d5b76242f84f21efdbf
mergehez <argesozdogan@gmail.com>
2025-11-30 00:26:56 +0100

    extract default debug-config to a variable

--- ui/src/utils/useAppConfig.ts
+++ ui/src/utils/useAppConfig.ts
@@ -1,33 +1,35 @@
 import {reactive} from "vue";
 import {defineStore} from "@/utils/utils.ts";
 
-function _useAppConfig() {
-    const config: typeof window.dokie = window.dokie ?? {
-        "currentHostname": location.host,
-        // "openApiJsonUrl": "http://127.0.0.1:8000/openapi",
-        "openApiJsonUrl": "https://localhost:35125/openapi/internal.json",
-        "hostnames": [],
-        "variables": {
-            "username": "mazlum",
-            "password": "my-password"
-        },
-        "headers": {
-            "x-api-key": "test-api-key",
-            "Content-Type": "application/json"
-        },
-        "postscripts": {
-            "GET /api/login": "envs.headers['x-api-key'] = response.data.value.ticket;",
-        },
-        "bodies": {
-            "GET /api/login": `{
+const debugConfig = {
+    "currentHostname": location.host,
+    "openApiJsonUrl": "http://127.0.0.1:8000/openapi",
+    // "openApiJsonUrl": "https://localhost:35125/openapi/internal.json",
+    "hostnames": [],
+    "variables": {
+        "username": "mazlum",
+        "password": "my-password"
+    },
+    "headers": {
+        "x-api-key": "test-api-key",
+        "Content-Type": "application/json"
+    },
+    "postscripts": {
+        "GET /api/login": "envs.headers['x-api-key'] = response.data.value.ticket;",
+    },
+    "bodies": {
+        "GET /api/login": `{
                 "email": "{{email}}",
                 "password": "{{password}}"
             }`,
-        },
-        "favorites": [
-            "GET /api/login",
-        ]
-    }
+    },
+    "favorites": [
+        "GET /api/login",
+    ]
+} as const satisfies typeof window.dokie;
+
+function _useAppConfig() {
+    const config: typeof window.dokie = window.dokie ?? debugConfig;
 
     config.currentHostname ||= location.host
     config.openApiJsonUrl ||= config.currentHostname + '/openapi/v1.json';
