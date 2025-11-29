69607e1ec80a427aaa3b47dc359423ef9e0853ee
mergehez <argesozdogan@gmail.com>
2025-11-30 00:07:03 +0100

    autosize on focus (commented again)

--- ui/src/components/ui/ArgInput.vue
+++ ui/src/components/ui/ArgInput.vue
@@ -1,23 +1,61 @@
 <script setup lang="ts">
 
-import {ElInput, type InputEmits, type InputProps} from "element-plus";
+import {ElInput} from "element-plus";
 
-type aa = InputEmits & InputProps;
 const modelValue = defineModel<string | undefined>({required: true})
 const props = defineProps<{
     placeholder?: string;
     inputStyle?: string;
+    autosize?: boolean;
 }>()
 
-// const emit = defineEmits<InputEmits>();
-// const reg
+// const autosize = ref<any>(props.autosize ? {minRows: 1, maxRows: 1} : false);
+// const uniqueKey = ref('aaa');
+// const refInput = useTemplateRef('refInput');
+// let keyWhenFocused = uniqueKey.value
+// let caretWhenFocused = 0;
+//
+// function onFocus() {
+//     if (!props.autosize || keyWhenFocused != uniqueKey.value) return;
+//
+//     const inp = refInput.value?.input || refInput.value?.textarea!;
+//     if (inp.scrollWidth <= inp.clientWidth) return;
+//
+//     setTimeout(() => {
+//         caretWhenFocused = refInput.value?.input?.selectionStart || refInput.value?.textarea?.selectionStart || 0;
+//
+//         keyWhenFocused = uniqueKey.value;
+//         autosize.value = {minRows: 1, maxRows: 5};
+//         uniqueKey.value = uniqueId();
+//         setTimeout(() => {
+//             refInput.value?.textarea?.focus();
+//             keyWhenFocused = uniqueKey.value
+//             if (refInput.value?.textarea) {
+//                 refInput.value.textarea.selectionStart = caretWhenFocused;
+//                 refInput.value.textarea.selectionEnd = caretWhenFocused;
+//             }
+//         }, 50);
+//     }, 100)
+// }
+//
+// function onBlur() {
+//     if (!props.autosize || keyWhenFocused != uniqueKey.value) return;
+//     // isFocused.value = false;
+//     autosize.value = {minRows: 1, maxRows: 1};
+//     uniqueKey.value = uniqueId();
+//     keyWhenFocused = uniqueKey.value
+// }
 </script>
 
 <template>
     <ElInput
-        v-model="modelValue"
+        :model-value="modelValue"
+        @update:model-value="v => modelValue = v"
         :input-style="inputStyle"
         class="bg-x1/50"
         :placeholder="placeholder"
+        :autosize="autosize"
+        :type="autosize ? 'textarea' : 'text'"
+        resize="none"
     />
 </template>
