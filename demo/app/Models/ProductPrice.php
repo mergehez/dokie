70e6fcad4ad750faf04cea5a75a14756d443a619
mergehez <argesozdogan@gmail.com>
2025-11-30 01:19:51 +0100

    add missing relationship (product) to ProductPrice

--- demo/app/Models/ProductPrice.php
+++ demo/app/Models/ProductPrice.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class ProductPrice extends Model
 {
     //
+
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
